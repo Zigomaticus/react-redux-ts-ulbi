f0ae99454c7938d0fdd68ae07179bba5b5ccc2a8
denis <permwed@gmail.com>
2022-04-03 20:19:58 +0500

    jsut a commit

--- src/store/actions-creaters/todo.ts
+++ src/store/actions-creaters/todo.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { Dispatch } from "redux";
+// Types
 import { TodoAction, TodoActionTypes } from "../../types/todo";
 
 export const fetchTodos = (page = 1, limit = 10) => {
